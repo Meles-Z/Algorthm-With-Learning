54eebcbc5f035411a3e7e389c75ab19db46ca6c3
Meles-Z <meles.zawdie@gmail.com>
2025-02-17 21:49:31 +0300

    filing stytem

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	// fileName:="test.txt"
+	// file, err:=os.OpenFile(fileName, os.O_CREATE| os.O_WRONLY, 0644)
+	// if err !=nil{
+	// 	fmt.Println("Error opening file:", err)
+	// 	return
+	// }
+	// defer file.Close()
+	// b:=[]byte("Hello test of write to file")
+	// n, err:=file.Write(b)
+	// if err !=nil{
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("Number of bytes written in file is: %d\n", n)
+	// if err !=nil{
+	// 	log.Fatal(err)
+	// }
+
+	// fileName := "test.txt"
+	// file, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0644)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer file.Close()
+	// _, err=file.WriteString("Hello, Namaste of workld!")
+	// if err !=nil{
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("Done writting into file")
+
+	fileName:="test.txt"
+	data:="Hi everyone! What about your work!"
+	 err:=ioutil.WriteFile(fileName, []byte(data), 0 )
+	 if err !=nil{
+        log.Fatal(err)
+     }
+	 fmt.Println("Done!")
+}
