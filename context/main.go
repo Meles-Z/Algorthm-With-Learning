14851db1ee4793ed28acd7e4e38bafe04334473c
Meles-Z <meles.zawdie@gmail.com>
2025-02-23 19:23:58 +0300

    pod allocation

--- context/main.go
+++ context/main.go
@@ -1,29 +1,52 @@
+// package main
+
+// import (
+// 	"context"
+// 	"fmt"
+// 	"time"
+// )
+
+// func main() {
+// 	ctx, cancel:=context.WithCancel(context.Background())
+
+// 	go Worker(ctx)
+// 	time.Sleep(2*time.Second)
+// 	cancel()
+// 	time.Sleep(2*time.Second)
+// }
+
+// func Worker (ctx context.Context){
+// 	for{
+// 		select{
+// 		case <-ctx.Done():
+// 			fmt.Println("Work canceled")
+// 			return
+// 		default:
+// 			fmt.Println("Working...")
+// 			time.Sleep(500*time.Microsecond)
+// 		}
+// 	}
+// }
+
 package main
 
 import (
 	"context"
-	"fmt"
-	"time"
 )
 
-func main() {
-	ctx, cancel:=context.WithCancel(context.Background())
-	
-	go Worker(ctx)
-	time.Sleep(2*time.Second)
-	cancel()
-	time.Sleep(2*time.Second)
-}
+func execute(ctx context.Context, fn func() int) (int, error) {
+	ch := make(chan int, 1)
+	go func() {
+		ch <- fn()
+	}()
 
-func Worker (ctx context.Context){
-	for{
-		select{
-		case <-ctx.Done():
-			fmt.Println("Work canceled")
-			return
-		default:
-			fmt.Println("Working...")
-			time.Sleep(500*time.Microsecond)
-		}
+	select {
+	case res := <-ch:
+		return res, nil
+	case <-ctx.Done():
+		return 0, ctx.Err()
 	}
+}
+func main() {
+
 }
