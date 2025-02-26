6c626d79acd27a34178766f43d35c76f19fa77db
Meles-Z <meles.zawdie@gmail.com>
2025-02-26 21:12:27 +0300

    changed main.go

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Person struct {
+	name  string
+	email string
+}
+type myint int
+type mystring string
+
+// when i use anonays function i can use type like below
+type myfunc func(n int)
+
+func increment(n int) {
+	fmt.Printf("The incremented Value is:%d", n+1)
+}
+
+func square(n int) {
+	fmt.Printf("The squeared value is:%d", n*n)
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	var f myint = 6
+	var t mystring = "Hi there!"
+	fmt.Println(t)
+	fmt.Println(f)
+	p := Person{
+		name:  "s",
+		email: "m",
+	}
+	fmt.Printf("%+v\n", p)
+
+	
+	var inc myfunc=increment
+	fmt.Println(inc)
+	inc=square
+	fmt.Println(inc)
 }
