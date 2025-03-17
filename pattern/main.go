95c060c951cf215c74a55676891f04bbb869859a
Meles-Z <meles.zawdie@gmail.com>
2025-03-17 17:26:41 +0300

    started luhan number

--- pattern/main.go
+++ pattern/main.go
@@ -28,7 +28,40 @@ func trianglePattern(n int) {
 	}
 }
 
+func isLuhan(nums int) bool {
+	num := sumOfDigit(nums)
+	if num/10 != 0 {
+		return false
+	}
+	return true
+}
+func sumOfDigit(n int) int {
+	store := []int{}
+	for n != 0 {
+		singleNumber := n % 10
+		store = append(store, singleNumber)
+		n /= 10
+	}
+	sum := doubleNumbers(n)
+	for _, v := range store {
+		sum += v
+	}
+	return sum
+}
+func doubleNumbers(n int) int {
+	double := n * 2
+	sum := 0
+	if double > 10 {
+		sum = 1 + n%10
+	} else {
+		sum = double
+	}
+	return sum
+}
+
 func main() {
 	// decreasePattern(5)
-	trianglePattern(4)
+	// trianglePattern(4)
+	fmt.Println(doubleNumbers(22))
+	fmt.Println(isLuhan(345))
 }
