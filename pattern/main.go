e0fbf01aa55923cacb116b6d592f6da6c4e54c45
Meles-Z <meles.zawdie@gmail.com>
2025-03-19 11:22:13 +0300

    diandomd pattern added

--- pattern/main.go
+++ pattern/main.go
@@ -46,9 +46,55 @@ func doubleNumbers(n int) int {
 	return sum
 }
 
+func centeredHash(rows int) {
+	// maxWidth := rows
+	for i := rows; i > 1; i-- {
+		if i%2 == 0 {
+			space := (rows - i) / 2
+			for s := 0; s < space; s++ {
+				fmt.Print(" ")
+			}
+			for j := 0; j < i; j++ {
+				fmt.Print("#")
+			}
+
+			fmt.Println()
+		}
+	}
+}
+
+func diamondShape(rows int) {
+	for i := 1; i < rows; i++ {
+		if i%2 == 0 {
+			space := (rows - i) / 2
+			for s := 0; s < space; s++ {
+				fmt.Print(" ")
+			}
+			for j := 0; j < i; j++ {
+				fmt.Print("#")
+			}
+
+			fmt.Println()
+		}
+	}
+	for k := rows-1; k > 1; k-- {
+		if k%2 == 0 {
+			space := (rows - k)/2
+			for s := 0; s < space; s++ {
+				fmt.Print(" ")
+			}
+			for j := 0; j <k; j++ {
+				fmt.Print("#")
+			}
+			fmt.Println()
+		}
+	}
+}
 func main() {
 	// decreasePattern(5)
 	// trianglePattern(4)
-	fmt.Println(doubleNumbers(349))
-	fmt.Println(isLuhan(349))
+	// fmt.Println(doubleNumbers(349))
+	// fmt.Println(isLuhan(349))
+	// centeredHash(8)
+	diamondShape(10)
 }
